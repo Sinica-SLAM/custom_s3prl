cf4e09093bbdc959f5d8e233db1c57388f1ad5ca
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2023-08-11 01:16:04 +0800

    Add attention pooling in the tf_featurizer.py

--- s3prl/upstream/tf_featurizer.py
+++ s3prl/upstream/tf_featurizer.py
@@ -14,6 +14,44 @@ from .interfaces import UpstreamBase, SAMPLE_RATE, TOLERABLE_SEQLEN_DIFF
 
 """use self-attention of all layers of upstream SLL model to get the feature"""
 
+class AttentionPooling(nn.Module):
+    def __init__(self, input_dim, proj_dim, select_id):
+        super().__init__()
+        self.input_dim = input_dim
+        self.proj_dim = proj_dim
+        self.select_id = select_id
+        self.Q = Linear(input_dim, proj_dim)
+        self.K = Linear(input_dim, proj_dim)
+
+    def forward(self, x): # (..., seq_len, input_dim)
+        select_layer = x[..., self.select_id, :] # (..., input_dim)
+        quiry_x = self.Q(x)                      # (..., seq_len, proj_dim)
+        key_x = self.K(select_layer)             # (..., proj_dim)
+        # compute attention weights by dot product of quiry and key
+        attn_weights = torch.matmul(quiry_x, key_x.unsqueeze(-1)).squeeze(-1) # (..., seq_len)
+        attn_weights = F.softmax(attn_weights / np.sqrt(self.proj_dim), dim=-1) # (..., seq_len)
+        # weighted sum of all layers
+        attn_x = torch.matmul(attn_weights.unsqueeze(-2), x).squeeze(-2) # (..., input_dim)
+        return attn_x
+
+class AttentionPooling2(nn.Module):
+    def __init__(self, input_dim, proj_dim, select_dim):
+        super().__init__()
+        self.input_dim = input_dim
+        self.proj_dim = proj_dim
+        self.select_dim = select_dim
+        self.proj = Linear(input_dim, proj_dim)
+
+        self.attn = nn.Parameter(torch.randn(proj_dim,1)) # (proj_dim, 1)
+
+    def forward(self, x):     # (..., seq_len, input_dim)
+        proj_x = self.proj(x) # (..., seq_len, proj_dim)
+        weights = torch.matmul(proj_x, self.attn).squeeze(-1) # (..., seq_len)
+        norm_weights = F.softmax(weights / np.sqrt(self.proj_dim), dim=-1) # (..., seq_len)
+        attn_x = torch.matmul(norm_weights.unsqueeze(-2), x).squeeze(-2) # (..., input_dim)
+        return attn_x
+
+
 class TFFeaturizer(nn.Module):
     def __init__(
         self,
@@ -25,16 +63,25 @@ class TFFeaturizer(nn.Module):
     ):
         super().__init__()
         self.name = "TFFeaturizer"
+        print("[TFFeaturizer] init")
+        assert layer_selection is not None, "layer_selection should be specified"
         self.layer_selection = layer_selection
         self.normalize = normalize
-        self.downsample_rate = upstream.get_downsample_rates()
-        self.output_dim = upstream.hidden_size
+        self.downsample_rate = upstream.get_downsample_rates("hidden_states")
         self.upstream_device = upstream_device
+        self.output_dim = self.get_output_dim(upstream, upstream_device)
+        self.atten_pooling = AttentionPooling2(self.output_dim, self.output_dim//2, self.layer_selection)
 
-        self.multihead_attn = MultiheadAttention(embed_dim=upstream.hidden_size,
-                                                    num_heads=4,
-                                                    bias=False,
-                                                    batch_first=True)
+    def get_output_dim(self, upstream: UpstreamBase, upstream_device: str):
+        upstream.eval()
+        paired_wavs = [torch.randn(SAMPLE_RATE).to(upstream_device)]
+        with torch.no_grad():
+            paired_features = upstream(paired_wavs)
+        feature = self._select_feature(paired_features)
+        if isinstance(feature, (list, tuple)):
+            feature = feature[0]
+        output_dim = feature.size(-1)
+        return output_dim
 
     def _select_feature(self, features):
         feature = features.get("hidden_states")
@@ -44,24 +91,14 @@ class TFFeaturizer(nn.Module):
 
         return feature
 
-    def _tf_select(self, features: List[Tensor]):
-        """select tenth vector after self-attention as the feature"""
-        features = torch.stack(features, dim=0) # L*(N, T, D) -> (L, N, T, D)
+    def _attn_sum(self, features: List[Tensor]):
+        """use attention pool the calculate the feature"""
+        features = torch.stack(features, dim=0)          # L*(N, T, D) -> (L, N, T, D)
         if self.normalize:
             features = F.layer_norm(features, features.shape[3:]) # (L, N, T, D)
-        L, N, T, D = features.shape
-        features = features.permute(2, 1, 0, 3) # (L, N, T, D) -> (T, N, L, D)
-        features = features.view(-1, L, D) # (T, N, L, D) -> (T*N, L, D)
-        attn_output, _ = self.multihead_attn(
-            features,
-            features,
-            features,
-            need_weights=False
-        ) # (T*N, L, D)
-        attn_output = attn_output.view(T, N, L, D) # (T*N, L, D) -> (T, N, L, D)
-        attn_output = attn_output.permute(2, 1, 0, 3) # (T, N, L, D) -> (L, N, T, D)
-        selected_feature = attn_output[self.layer_selection] # (L, N, T, D) -> (N, T, D)
-        return selected_feature
+        prmt_features = features.permute(2, 1, 0, 3)     # (L, N, T, D) -> (T, N, L, D)
+        attn_feature = self.atten_pooling(prmt_features) # (T, N, L, D) -> (T, N, D)
+        return attn_feature.permute(1, 0, 2)             # (T, N, D) -> (N, T, D)
 
     def tolist(self, paired_wavs: List[Tensor], paired_feature: Tensor):
         assert paired_feature.dim() == 3, "(batch_size, max_seq_len, feat_dim)"
@@ -83,6 +120,6 @@ class TFFeaturizer(nn.Module):
     ):
         features = self._select_feature(paired_features)
         assert isinstance(features, (list|tuple)), "features should be a list of tensors"
-        feature = self._tf_select(features)
+        feature = self._attn_sum(features)
 
         return self.tolist(paired_wavs, feature)

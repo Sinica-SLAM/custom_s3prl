654057f7340955373674b759cc6fbaba59a7dd83
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2023-08-13 00:36:17 +0800

    Add attention pooling 3

--- s3prl/upstream/tf_featurizer.py
+++ s3prl/upstream/tf_featurizer.py
@@ -15,13 +15,13 @@ from .interfaces import UpstreamBase, SAMPLE_RATE, TOLERABLE_SEQLEN_DIFF
 """use self-attention of all layers of upstream SLL model to get the feature"""
 
 class AttentionPooling(nn.Module):
-    def __init__(self, input_dim, proj_dim, select_id):
+    def __init__(self, input_dim, select_id):
         super().__init__()
         self.input_dim = input_dim
-        self.proj_dim = proj_dim
+        self.proj_dim = input_dim // 2
         self.select_id = select_id
-        self.Q = Linear(input_dim, proj_dim)
-        self.K = Linear(input_dim, proj_dim)
+        self.Q = Linear(input_dim, self.proj_dim)
+        self.K = Linear(input_dim, self.proj_dim)
 
     def forward(self, x): # (..., seq_len, input_dim)
         select_layer = x[..., self.select_id, :] # (..., input_dim)
@@ -35,14 +35,13 @@ class AttentionPooling(nn.Module):
         return attn_x
 
 class AttentionPooling2(nn.Module):
-    def __init__(self, input_dim, proj_dim, select_dim):
+    def __init__(self, input_dim, _):
         super().__init__()
         self.input_dim = input_dim
-        self.proj_dim = proj_dim
-        self.select_dim = select_dim
-        self.proj = Linear(input_dim, proj_dim)
+        self.proj_dim = input_dim // 2
+        self.proj = Linear(input_dim, self.proj_dim)
 
-        self.attn = nn.Parameter(torch.randn(proj_dim,1)) # (proj_dim, 1)
+        self.attn = nn.Parameter(torch.randn(self.proj_dim,1)) # (proj_dim, 1)
 
     def forward(self, x):     # (..., seq_len, input_dim)
         proj_x = self.proj(x) # (..., seq_len, proj_dim)
@@ -50,6 +49,33 @@ class AttentionPooling2(nn.Module):
         norm_weights = F.softmax(weights / np.sqrt(self.proj_dim), dim=-1) # (..., seq_len)
         attn_x = torch.matmul(norm_weights.unsqueeze(-2), x).squeeze(-2) # (..., input_dim)
         return attn_x
+
+class AttentionPooling3(nn.Module):
+    """Multihead version of AttentionPooling2"""
+    def __init__(self, idim, num_heads = 4):
+        super().__init__()
+        assert idim % num_heads == 0, "proj_dim should be divisible by num_heads"
+        vdim = idim // num_heads
+        kdim = vdim // 2
+        self.idim = idim
+        self.kdim = kdim
+        self.vdim = vdim
+        self.num_heads = num_heads
+
+        self.Q   = nn.Parameter(torch.randn(num_heads, vdim, kdim)) # (num_heads, kdim, kdim)
+        self.key = nn.Parameter(torch.randn(num_heads, kdim,    1)) # (num_heads, kdim,    1)
+
+    def forward(self, x : Tensor): # (..., seq_len, idim)
+        split_x = x.reshape(*x.shape[:-1], self.num_heads, self.vdim) # (..., seq_len, num_heads, vdim)
+        split_x = split_x.transpose(-2, -3) # (..., num_heads, seq_len, vdim)
+        quiry_x = torch.matmul(split_x, self.Q) # (..., num_heads, seq_len, kdim)
+        # compute attention weights by dot product of quiry and key
+        attn_weights = torch.matmul(quiry_x, self.key).squeeze(-1) # (..., num_heads, seq_len)
+        attn_weights = F.softmax(attn_weights / np.sqrt(self.kdim), dim=-1) # (..., num_heads, seq_len)
+        # weighted sum of all layers
+        split_y = torch.matmul(attn_weights.unsqueeze(-2), split_x).squeeze(-2) # (..., num_heads, vdim)
+        y = split_y.reshape(*x.shape[:-2], self.idim) # (..., idim)
+        return y
 
 
 class TFFeaturizer(nn.Module):
@@ -70,7 +96,7 @@ class TFFeaturizer(nn.Module):
         self.downsample_rate = upstream.get_downsample_rates("hidden_states")
         self.upstream_device = upstream_device
         self.output_dim = self.get_output_dim(upstream, upstream_device)
-        self.atten_pooling = AttentionPooling2(self.output_dim, self.output_dim//2, self.layer_selection)
+        self.atten_pooling = AttentionPooling2(self.output_dim, self.layer_selection)
 
     def get_output_dim(self, upstream: UpstreamBase, upstream_device: str):
         upstream.eval()

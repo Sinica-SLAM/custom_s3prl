dd04dd9587c900d1f09062dca64c4cf2afdeb541
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2023-08-23 01:26:15 +0800

    Use contexting manager to manage the cache

--- s3prl/downstream/cache.py
+++ s3prl/downstream/cache.py
@@ -1,48 +1,60 @@
 import os
 import numpy as np
-from typing import List, Tuple, Callable, Any
+from typing import List, Tuple, Callable, Any, Optional
 from functools import wraps
 import multiprocessing.dummy as mp
 from pathlib import Path
 import h5py as h5
 
 import torch
-from torch import nn
 from torch import Tensor
 
+from timeit import default_timer as timer
 
-class CacheModule:
+def timeit(tolerance=0.1):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start = timer()
+            result = func(*args, **kwargs)
+            end = timer()
+            duration = end - start
+            if duration > tolerance:
+                print(f'\n[{func.__name__}] - {duration:.2f}s!!')
+            return result
+        return wrapper
+    return decorator
+
+WAIT_FOR_SAVE = 64
+
+class CacheManager:
     def __init__(self,
                  process_func: Callable,
-                 cache_path: str,
                  device: str,
+                 cache_path: Optional[str] = None,
                  use_cache: bool = True,
-                 num_worker: int = None,
-                 sep: str = '-'):
+                 num_worker: int = None):
         self.process_func = process_func
-        self.cache_path = Path(cache_path)
+        self.cache_path = Path(cache_path or f'data/cache/{process_func.__name__}.h5')
         self.device = device
         self.use_cache = use_cache
-        self.num_worker = num_worker or os.cpu_count()
-        self.sep = sep
+        self.num_worker = num_worker or min(8, os.cpu_count())
 
+    def __enter__(self):
         if self.use_cache:
             self.cache_path.parent.mkdir(parents=True, exist_ok=True)
-            assert len(self.sep) == 1, f'seq must be a single character, got {self.sep}'
 
             self.cache_writer = h5.File(self.cache_path, 'a')
             self.cache_reader = self.cache_writer
             self.pool = mp.Pool(self.num_worker)
-            self.saving_features = None
+            self.saving_features = []
 
-            print(f"[CacheModule] - Use cache at {cache_path}")
+            print(f"[CacheModule] - Use cache at {self.cache_path}")
         else:
             print(f"[CacheModule] - Don't use cache")
+        return self
 
-    def __del__(self):
-        self.close()
-
-    def close(self):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         if hasattr(self, 'pool') and self.pool:
             self.pool.close()
             self.pool.join()
@@ -50,74 +62,63 @@ class CacheModule:
         if hasattr(self, 'cache_writer') and self.cache_writer:
             self.cache_writer.close()
             del self.cache_writer
-        if hasattr(self, 'cache_reader') and self.cache_reader:
-            self.cache_reader.close()
             del self.cache_reader
         if hasattr(self, 'saving_features') and self.saving_features:
-            self.saving_features.get()
+            for saving_feature in self.saving_features:
+                saving_feature.get()
             del self.saving_features
 
     def _parse_cache_path(self, wavname):
-        return wavname.replace(self.sep, '/')
-
-    def have_cached(self, wavname):
-        try:
-            return self._parse_cache_path(wavname) in self.cache_reader
-        except:
-            print(f'Error when checking cache for {wavname}')
-            self.close()
-            raise
+        return wavname
 
     def _save_cache(self, wavname, feature):
-        if not self.have_cached(wavname):
-            np_feature = feature.cpu().numpy()
-            feature_path = self._parse_cache_path(wavname)
+        np_feature = feature.cpu().numpy()
+        feature_path = self._parse_cache_path(wavname)
+        try:
             self.cache_writer.create_dataset(feature_path, data=np_feature, compression='lzf', shuffle=True)
+        except RuntimeError:
+            print(f'Failed to save {feature_path}')
 
     def async_save_caches(self, wavnames: List[str], features: List[Tensor]):
-        if self.saving_features:
-            self.saving_features.get()
-        self.saving_features = self.pool.starmap_async(self._save_cache, zip(wavnames, features))
+        while len(self.saving_features) > WAIT_FOR_SAVE:
+            self.saving_features.pop(0).get()
+        for wavname, feature in zip(wavnames, features):
+            self.saving_features.append(self.pool.apply_async(self._save_cache, (wavname, feature)))
 
     def _load_cache(self, wavname):
         cache_path = self._parse_cache_path(wavname)
-        return self.cache_reader[cache_path][:]
+        if (feature := self.cache_reader.get(cache_path)) is not None:
+            return feature[:]
+        else:
+            return None
 
     def with_cache(self, func):
         @wraps(func)
         def wrapper(wavpath: str):
             wavname = Path(wavpath).stem
-            return self._load_cache(wavname) \
-                if self.have_cached(wavname) \
-                else func(wavpath)
+            feature = self._load_cache(wavname)
+            return func(wavpath) if feature is None else feature
         return wrapper if self.use_cache else func
 
-    def _to_tensor(self, array, *args, **kwargs):
-         return torch.FloatTensor(array).to(self.device, *args, **kwargs)
-
-    def get_features(self, wavs: List[np.ndarray], wavnames: List[str], save=True) -> List[Tensor]:
-        # use cache data if have cached
+    def get_features(self, wavs: List[Tensor], wavnames: List[str], save=True) -> List[Tensor]:
         cached_states = [wav.ndim != 1 for wav in wavs]
-        cached_datas = [(wav, wavname) for wav, wavname, cached in zip(wavs, wavnames, cached_states) if cached]
-        uncached_datas = [(wav, wavname) for wav, wavname, cached in zip(wavs, wavnames, cached_states) if not cached]
 
-        # move uncached wavs to device
-        uncached_features = []
-        if uncached_datas:
-            uncached_wavs, uncached_names = zip(*uncached_datas)
-            uncached_wavs = [self._to_tensor(w, non_blocking=True) for w in uncached_wavs]
-
-        # move cached features to device
         cached_features = []
-        if cached_datas:
-            cached_features, _ = zip(*cached_datas)
-            cached_features = [self._to_tensor(f, non_blocking=True) for f in cached_features]
+        uncached_wavs, uncached_names = [], []
+        for wav, wavname, cached in zip(wavs, wavnames, cached_states):
+            if cached:
+                cached_features.append(wav)
+            else:
+                uncached_wavs.append(wav)
+                uncached_names.append(wavname)
+
+        if not self.use_cache:
+            assert len(cached_features) == 0, "cached data is not empty when not using cache"
 
         # process uncached data
-        if uncached_datas:
-            uncached_features = self.process_func(uncached_wavs)
-            if self.use_cache and save:
-                self.async_save_caches(uncached_names, uncached_features)
+        uncached_features = self.process_func(uncached_wavs) if uncached_wavs else []
+        if self.use_cache and save and uncached_features:
+            self.async_save_caches(uncached_names, uncached_features)
 
         # merge cached and uncached data
         features = []
--- s3prl/downstream/runner_cache.py
+++ s3prl/downstream/runner_cache.py
@@ -24,7 +24,7 @@ from s3prl import hub
 from s3prl.optimizers import get_optimizer
 from s3prl.schedulers import get_scheduler
 from s3prl.upstream.featurizer import Featurizer
-from s3prl.downstream.cache import CacheModule
+from s3prl.downstream.cache import CacheManager
 from s3prl.utility.helper import is_leader_process, get_model_state, show, defaultdict
 
 from huggingface_hub import HfApi, HfFolder, Repository
@@ -97,14 +97,8 @@ class RunnerCache():
         self.downstream = self._get_downstream()
         self.all_entries = [self.upstream, self.featurizer, self.downstream]
 
-    def __enter__(self):
-        self.cache = self._get_cache()
-        return self
+        self.cache_manager = self._get_cache()
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        if hasattr(self, 'cache') and self.cache:
-            self.cache.close()
-            del self.cache
 
     def _load_weight(self, model, name):
         init_weight = self.init_ckpt.get(name)
@@ -214,7 +208,7 @@ class RunnerCache():
 
         cache_path = Path(libri_root)/"cache"/upstream_name/dataset_name/f"{layer}.h5"
         use_cache = not self.upstream.trainable and not self.featurizer.trainable and self.args.use_cache
-        return CacheModule(self.process_wavs, cache_path, self.args.device, use_cache=use_cache)
+        return CacheManager(self.process_wavs, self.args.device, cache_path, use_cache=use_cache)
 
 
     def _get_downstream(self):
@@ -265,18 +259,17 @@ class RunnerCache():
             self.downstream.model.get_dataloader(split) # create dataset
         split_dataset = getattr(self.downstream.model, f"{split}_dataset")
         if not hasattr(split_dataset, "_have_wrap_cache"):
-            split_dataset._load_wav = self.cache.with_cache(split_dataset._load_wav)
+            split_dataset._load_wav = self.cache_manager.with_cache(split_dataset._load_wav)
             setattr(split_dataset, "_have_wrap_cache", True)
 
     def process_wavs(self, wavs: List[Tensor]) -> List[Tensor]:
-        if self.upstream.trainable and self.training:
+        if self.upstream.trainable:
             features = self.upstream.model(wavs)
         else:
             with torch.no_grad():
                 features = self.upstream.model(wavs)
 
-        if (self.upstream.trainable or self.featurizer.trainable) \
-            and self.training:
+        if self.upstream.trainable or self.featurizer.trainable:
             features = self.featurizer.model(wavs, features)
         else:
             with torch.no_grad():
@@ -330,136 +323,138 @@ class RunnerCache():
         # wrap dataset
         self.wrap_dataset(train_split)
 
-        while pbar.n < pbar.total:
-            try:
-                dataloader = self.downstream.model.get_dataloader(train_split, epoch=epoch)
-            except TypeError as e:
-                if "unexpected keyword argument 'epoch'" in str(e):
-                    dataloader = self.downstream.model.get_dataloader(train_split)
-                    if hasattr(dataloader, "sampler") and isinstance(dataloader.sampler, DistributedSampler):
-                        dataloader.sampler.set_epoch(epoch)
-                else:
-                    raise
-
-            for batch_id, (wavs, labels, wavnames) in enumerate(tqdm(dataloader, dynamic_ncols=True, desc='train', file=tqdm_file)):
-                # try/except block for forward/backward
+        with self.cache_manager as cache:
+            while pbar.n < pbar.total:
                 try:
-                    if pbar.n >= pbar.total:
-                        break
-                    global_step = pbar.n + 1
-
-                    self.training = True
-                    features = self.cache.get_features(wavs, wavnames)
-
-                    if specaug:
-                        features, _ = specaug(features)
-
-                    loss = self.downstream.model(
-                        train_split,
-                        features, labels, wavnames,
-                        records = records,
-                    )
-                    batch_ids.append(batch_id)
-
-                    gradient_accumulate_steps = self.config['runner'].get('gradient_accumulate_steps')
-                    (loss / gradient_accumulate_steps).backward()
-                    del loss
-
-                except RuntimeError as e:
-                    if 'CUDA out of memory' in str(e):
-                        print(f'[Runner] - CUDA out of memory at step {global_step}')
-                        if is_initialized():
-                            raise
-                        with torch.cuda.device(self.args.device):
-                            torch.cuda.empty_cache()
-                        optimizer.zero_grad()
-                        continue
+                    dataloader = self.downstream.model.get_dataloader(train_split, epoch=epoch)
+                except TypeError as e:
+                    if "unexpected keyword argument 'epoch'" in str(e):
+                        dataloader = self.downstream.model.get_dataloader(train_split)
+                        if hasattr(dataloader, "sampler") and isinstance(dataloader.sampler, DistributedSampler):
+                            dataloader.sampler.set_epoch(epoch)
                     else:
                         raise
 
-                # whether to accumulate gradient
-                backward_steps += 1
-                if backward_steps % gradient_accumulate_steps > 0:
-                    continue
+                for batch_id, (wavs, labels, wavnames) in enumerate(tqdm(dataloader, dynamic_ncols=True, desc='train', file=tqdm_file)):
+                    # try/except block for forward/backward
+                    try:
+                        if pbar.n >= pbar.total:
+                            break
+                        global_step = pbar.n + 1
 
-                # gradient clipping
-                grad_norm = torch.nn.utils.clip_grad_norm_(
-                    trainable_paras, self.config['runner']['gradient_clipping'])
+                        wavs = [torch.FloatTensor(wav).to(self.args.device, non_blocking=True) for wav in wavs]
 
-                # optimize
-                if math.isnan(grad_norm):
-                    print(f'[Runner] - grad norm is NaN at step {global_step}')
-                else:
-                    optimizer.step()
-                optimizer.zero_grad()
+                        features = cache.get_features(wavs, wavnames)
 
-                # adjust learning rate
-                if scheduler:
-                    scheduler.step()
+                        if specaug:
+                            features, _ = specaug(features)
 
-                if not is_leader_process():
-                    batch_ids = []
-                    records = defaultdict(list)
-                    continue
+                        loss = self.downstream.model(
+                            train_split,
+                            features, labels, wavnames,
+                            records = records,
+                        )
+                        batch_ids.append(batch_id)
 
-                # logging
-                if global_step % self.config['runner']['log_step'] == 0:
-                    self.downstream.model.log_records(
-                        train_split,
-                        records = records,
-                        logger = logger,
-                        global_step = global_step,
-                        batch_ids = batch_ids,
-                        total_batch_num = len(dataloader),
-                    )
-                    batch_ids = []
-                    records = defaultdict(list)
+                        gradient_accumulate_steps = self.config['runner'].get('gradient_accumulate_steps')
+                        (loss / gradient_accumulate_steps).backward()
+                        del loss
 
-                # evaluation and save checkpoint
-                save_names = []
+                    except RuntimeError as e:
+                        if 'CUDA out of memory' in str(e):
+                            print(f'[Runner] - CUDA out of memory at step {global_step}')
+                            if is_initialized():
+                                raise
+                            with torch.cuda.device(self.args.device):
+                                torch.cuda.empty_cache()
+                            optimizer.zero_grad()
+                            continue
+                        else:
+                            raise
 
-                if global_step % self.config['runner']['eval_step'] == 0:
-                    for split in self.config['runner']['eval_dataloaders']:
-                        save_names += self.evaluate(split, logger, global_step)
+                    # whether to accumulate gradient
+                    backward_steps += 1
+                    if backward_steps % gradient_accumulate_steps > 0:
+                        continue
 
-                if global_step % self.config['runner']['save_step'] == 0:
-                    def check_ckpt_num(directory):
-                        max_keep = self.config['runner']['max_keep']
-                        ckpt_pths = glob.glob(f'{directory}/states-*.ckpt')
-                        if len(ckpt_pths) >= max_keep:
-                            ckpt_pths = sorted(ckpt_pths, key=lambda pth: int(pth.split('-')[-1].split('.')[0]))
-                            for ckpt_pth in ckpt_pths[:len(ckpt_pths) - max_keep + 1]:
-                                os.remove(ckpt_pth)
-                    check_ckpt_num(self.args.expdir)
-                    save_names.append(f'states-{global_step}.ckpt')
+                    # gradient clipping
+                    grad_norm = torch.nn.utils.clip_grad_norm_(
+                        trainable_paras, self.config['runner']['gradient_clipping'])
 
-                if len(save_names) > 0:
-                    all_states = {
-                        'Optimizer': optimizer.state_dict(),
-                        'Step': global_step,
-                        'Epoch': epoch,
-                        'Args': self.args,
-                        'Config': self.config,
-                    }
+                    # optimize
+                    if math.isnan(grad_norm):
+                        print(f'[Runner] - grad norm is NaN at step {global_step}')
+                    else:
+                        optimizer.step()
+                    optimizer.zero_grad()
 
-                    for entry in self.all_entries:
-                        if entry.trainable:
-                            all_states[entry.name] = get_model_state(entry.model)
-
+                    # adjust learning rate
                     if scheduler:
-                        all_states['Scheduler'] = scheduler.state_dict()
+                        scheduler.step()
 
-                    if is_initialized():
-                        all_states['WorldSize'] = get_world_size()
+                    if not is_leader_process():
+                        batch_ids = []
+                        records = defaultdict(list)
+                        continue
 
-                    save_paths = [os.path.join(self.args.expdir, name) for name in save_names]
-                    tqdm.write(f'[Runner] - Save the checkpoint to:')
-                    for i, path in enumerate(save_paths):
-                        tqdm.write(f'{i + 1}. {path}')
-                        torch.save(all_states, path)
+                    # logging
+                    if global_step % self.config['runner']['log_step'] == 0:
+                        self.downstream.model.log_records(
+                            train_split,
+                            records = records,
+                            logger = logger,
+                            global_step = global_step,
+                            batch_ids = batch_ids,
+                            total_batch_num = len(dataloader),
+                        )
+                        batch_ids = []
+                        records = defaultdict(list)
 
-                pbar.update(1)
-            epoch += 1
+                    # evaluation and save checkpoint
+                    save_names = []
+
+                    if global_step % self.config['runner']['eval_step'] == 0:
+                        for split in self.config['runner']['eval_dataloaders']:
+                            save_names += self.evaluate(split, logger, global_step)
+
+                    if global_step % self.config['runner']['save_step'] == 0:
+                        def check_ckpt_num(directory):
+                            max_keep = self.config['runner']['max_keep']
+                            ckpt_pths = glob.glob(f'{directory}/states-*.ckpt')
+                            if len(ckpt_pths) >= max_keep:
+                                ckpt_pths = sorted(ckpt_pths, key=lambda pth: int(pth.split('-')[-1].split('.')[0]))
+                                for ckpt_pth in ckpt_pths[:len(ckpt_pths) - max_keep + 1]:
+                                    os.remove(ckpt_pth)
+                        check_ckpt_num(self.args.expdir)
+                        save_names.append(f'states-{global_step}.ckpt')
+
+                    if len(save_names) > 0:
+                        all_states = {
+                            'Optimizer': optimizer.state_dict(),
+                            'Step': global_step,
+                            'Epoch': epoch,
+                            'Args': self.args,
+                            'Config': self.config,
+                        }
+
+                        for entry in self.all_entries:
+                            if entry.trainable:
+                                all_states[entry.name] = get_model_state(entry.model)
+
+                        if scheduler:
+                            all_states['Scheduler'] = scheduler.state_dict()
+
+                        if is_initialized():
+                            all_states['WorldSize'] = get_world_size()
+
+                        save_paths = [os.path.join(self.args.expdir, name) for name in save_names]
+                        tqdm.write(f'[Runner] - Save the checkpoint to:')
+                        for i, path in enumerate(save_paths):
+                            tqdm.write(f'{i + 1}. {path}')
+                            torch.save(all_states, path)
+
+                    pbar.update(1)
+                epoch += 1
 
         pbar.close()
 
@@ -494,9 +489,6 @@ class RunnerCache():
             trainings.append(entry.model.training)
             entry.model.eval()
 
-        # wrap dataset
-        self.wrap_dataset(split)
-
         # prepare data
         dataloader = self.downstream.model.get_dataloader(split)
         evaluate_ratio = float(self.config["runner"].get("evaluate_ratio", 1))
@@ -504,17 +496,18 @@ class RunnerCache():
 
         batch_ids = []
         records = defaultdict(list)
-        for batch_id, (wavs, labels, wavnames) in enumerate(tqdm(dataloader, dynamic_ncols=True, desc=split, total=evaluate_steps)):
+        for batch_id, (wavs, *others) in enumerate(tqdm(dataloader, dynamic_ncols=True, desc=split, total=evaluate_steps)):
             if batch_id > evaluate_steps:
                 break
 
-            self.training = False
-            features = self.cache.get_features(wavs, wavnames)
+            wavs = [torch.FloatTensor(wav).to(self.args.device) for wav in wavs]
 
             with torch.no_grad():
+                features = self.process_wavs(wavs)
+
                 self.downstream.model(
                     split,
-                    features, labels, wavnames,
+                    features, *others,
                     records = records,
                     batch_id = batch_id,
                 )
@@ -635,4 +628,3 @@ class RunnerCache():
         print("[Runner] - Pushing model files to the Hub ...")
         model_repo.push_to_hub()
         print("[Runner] - Training run complete!")
-        
